06b22f602f50a6d01363ce906121150b7b80504d
Coco <corentin.recanzone@epitech.eu>
2023-12-19 12:12:09 +0100

    fix: ff

--- src-tauri/build.sh
+++ src-tauri/build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Get current branch
-branch=$(git rev-parse --abbrev-ref HEAD) # machine local
-branch2="${GITHUB_REF//refs\/heads\//}" # ci github actions
-
-chmod -R 777 /app/src-tauri/gen/android/app/src/main/jniLibs
-
-if [[ $branch == "main" || $branch2 == "main" ]]; then
-    echo "build launcher to branch : $branch"
-    npm run build:vite:develop
-else
-    echo "build launcher to branch : $branch"
-    npm run build:vite:develop
+if [ -d "/app/src-tauri/gen/android/app/src/main/jniLibs" ]; then
+  echo "SET CHMOD 777 !!!!!!!!!!!!!"
+  chmod -R 777 /app/src-tauri/gen/android/app/src/main/jniLibs
 fi
 
-chmod -R 777 /app/src-tauri/gen/android/app/src/main/jniLibs
+npm run build:vite:develop
+
+if [ -d "/app/src-tauri/gen/android/app/src/main/jniLibs" ]; then
+  echo "SET CHMOD 777 !!!!!!!!!!!!!"
+  chmod -R 777 /app/src-tauri/gen/android/app/src/main/jniLibs
+fi
